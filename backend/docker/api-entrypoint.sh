ad786008c1500c0257d6eaed24d35047be1c11ed
RajeevBaniya <baniyarajeev811@gmail.com>
2026-07-07 18:54:20 +0530

    Untrack local environment templates and containerization files

--- backend/docker/api-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-exec uvicorn src.main:app \
-  --host 0.0.0.0 \
-  --port "${BACKEND_PORT:-8000}" \
-  --timeout-graceful-shutdown 30 \
-  --lifespan on
--- backend/docker/celery-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eu
-
-exec celery -A src.jobs.celeryApp:celery_app worker \
-  --loglevel="${CELERY_LOG_LEVEL:-info}" \
-  --concurrency="${CELERY_CONCURRENCY:-2}" \
-  --queues="${CELERY_QUEUES:-recallstack_default}" \
-  --hostname="${CELERY_HOSTNAME:-recallstack-worker@%h}"
